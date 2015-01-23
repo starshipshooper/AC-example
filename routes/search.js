6c19b5a87201604fb13af479a74499d8212bb83e
adnan <adnanabdulle@adnans-mac-mini.home>
2015-01-23 12:45:37 +0000

    Fixed search

--- routes/search.js
+++ routes/search.js
@@ -10,22 +10,21 @@ router.get('/', function(req, res) {
   res.render('carSearcher');
 });
 
+var badImage;
+request('http://vcache.arnoldclark.com/imageserver/', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    badImage = body;
+  }
+})
+
 /*POST to search */
 router.post('/', function(req, res) {
   if (req.body.registrationplate && req.body.stocknumber) {
-  var badImage;
   var availableImages = [];
-  request('http://vcache.arnoldclark.com/imageserver/', function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      badImage = body; // Print the google web page.
-    }
-  })
    var uri = "http://vcache.arnoldclark.com/imageserver/"+obfuscate(req.body.stocknumber,req.body.registrationplate);
    var images = [uri+"/350/f",uri+"/350/i",uri+"/350/r",uri+"/350/4",uri+"/350/5",uri+"/350/6" ];
     async.eachSeries(images, function(item, mycallback){
       request(item, function (error, response, body) {
-        console.log(uri);
-        console.log(item);
         if (!error && response.statusCode == 200) {
           if (body!==badImage) {
                   availableImages.push(item)
